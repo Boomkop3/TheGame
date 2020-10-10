fb8a7eaf0f41f3fabab4297ad39304ed74ed10b8
Boomkop3 <sjors.de.bruijn@gmail.com>
2020-10-10 17:08:45 +0200

    refactor

---
+++ src/TestingChamber/Assets/ext.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public static class ext
+{
+    public enum Direction
+    {
+        RIGHT, LEFT, UP, DOWN, BACK, FORTH
+    }
+    public static void move (this GameObject obj, Direction direction, float speed)
+    {
+        switch (direction)
+        {
+            case Direction.BACK:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y,
+                        obj.transform.position.z - speed
+                    );
+                    break;
+                }
+            case Direction.FORTH:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y,
+                        obj.transform.position.z + speed
+                    );
+                    break;
+                }
+            case Direction.LEFT: 
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x - speed,
+                        obj.transform.position.y,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.RIGHT:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x + speed,
+                        obj.transform.position.y,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.UP:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y + speed,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.DOWN:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y - speed,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+        }
+    }
+}
--- src/TestingChamber/Assets/walker.cs
+++ src/TestingChamber/Assets/walker.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class walker : MonoBehaviour
 {
     public static float speed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,77 +21,9 @@ public class walker : MonoBehaviour
         bool back = Input.GetKey(KeyCode.S);
         bool forth = Input.GetKey(KeyCode.W);
 
-        if (right) gameObject.move(ext.Direction.RIGHT);
-        if (left) gameObject.move(ext.Direction.LEFT);
-        if (back) gameObject.move(ext.Direction.BACK);
-        if (forth) gameObject.move(ext.Direction.FORTH);
-    }
-}
-
-public static class ext
-{
-    public enum Direction
-    {
-        RIGHT, LEFT, UP, DOWN, BACK, FORTH
-    }
-    public static void move (this GameObject obj, Direction direction)
-    {
-        switch (direction)
-        {
-            case Direction.BACK:
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x,
-                        obj.transform.position.y,
-                        obj.transform.position.z - walker.speed
-                    );
-                    break;
-                }
-            case Direction.FORTH:
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x,
-                        obj.transform.position.y,
-                        obj.transform.position.z + walker.speed
-                    );
-                    break;
-                }
-            case Direction.LEFT: 
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x - walker.speed,
-                        obj.transform.position.y,
-                        obj.transform.position.z
-                    );
-                    break;
-                }
-            case Direction.RIGHT:
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x + walker.speed,
-                        obj.transform.position.y,
-                        obj.transform.position.z
-                    );
-                    break;
-                }
-            case Direction.UP:
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x,
-                        obj.transform.position.y + walker.speed,
-                        obj.transform.position.z
-                    );
-                    break;
-                }
-            case Direction.DOWN:
-                {
-                    obj.transform.position = new Vector3(
-                        obj.transform.position.x,
-                        obj.transform.position.y - walker.speed,
-                        obj.transform.position.z
-                    );
-                    break;
-                }
-        }
+        if (right) gameObject.move(ext.Direction.RIGHT, speed);
+        if (left) gameObject.move(ext.Direction.LEFT, speed);
+        if (back) gameObject.move(ext.Direction.BACK, speed);
+        if (forth) gameObject.move(ext.Direction.FORTH, speed);
     }
 }
