bd7a5afa7bf8b983896666571d83eb1a8be7aa47
Boomkop3 <sjors.de.bruijn@gmail.com>
2020-10-10 13:49:23 +0200

    Added Sjors' test/sketch project

---
+++ src/TestingChamber/Assets/simple_spritesheet_animator.cs
@@ -0,0 +1,99 @@
+﻿#define debug
+#undef debug
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class simple_spritesheet_animator : MonoBehaviour
+{
+    public int num_sprites_width = 1;
+    public int num_sprites_height = 1;
+    public int framerate = 15;
+
+    private Material material;
+    private Vector2 scale;
+    private float timeDelta = 0;
+    private float frametime;
+
+    private int spriteX, spriteY = 0;
+    private Vector2 offset;
+    private float spriteFractionWidth, spriteFractionHeight;
+
+    void Start()
+    {
+        // get material
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        material = meshRenderer.material;
+        // calculate scale
+        spriteFractionWidth = 1.0f / num_sprites_width;
+        spriteFractionHeight = 1.0f / num_sprites_height;
+        scale = new Vector2(
+            spriteFractionWidth,
+            spriteFractionHeight
+        );
+        // calculate frametime
+        frametime = 1.0f / framerate;
+        // Set first offset
+        offset = getOffset(spriteX, spriteY);
+        /*
+        offset = new Vector2(
+            0.0f, 1.0f - spriteFractionHeight
+        );
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if debug
+        spriteFractionWidth = 1.0f / num_sprites_width;
+            spriteFractionHeight = 1.0f / num_sprites_height;
+            scale = new Vector2(
+                spriteFractionWidth,
+                spriteFractionHeight
+            );
+            frametime = 1.0f / framerate;
+#endif
+        material.mainTextureScale = scale;
+        material.mainTextureOffset = offset;
+        timeDelta += Time.deltaTime;
+        if (timeDelta >= frametime)
+        {
+            nextFrame();
+            timeDelta -= frametime;
+        }
+    }
+
+    void nextFrame()
+    {
+        spriteX += 1;
+        if (spriteX == num_sprites_width)
+        {
+            spriteX = 0;
+            spriteY += 1;
+            if (spriteY == num_sprites_height)
+            {
+                spriteY = 0;
+            }
+        }
+        offset = getOffset(spriteX, spriteY);
+    }
+
+    Vector2 getOffset(int x, int y)
+    {
+        float uvX = x * spriteFractionWidth;
+        float uvY = ((num_sprites_height - y) * spriteFractionHeight);
+#if debug
+        Debug.Log("AnimX: " + x + " AnimY: " + y);
+        Debug.Log("x: " + uvX);
+        Debug.Log("y: " + uvY);
+#endif
+        return new Vector2(
+            uvX,
+            uvY
+        );
+    }
+}
---
+++ src/TestingChamber/Assets/walker.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class walker : MonoBehaviour
+{
+    public static float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool right = Input.GetKey(KeyCode.D);
+        bool left = Input.GetKey(KeyCode.A);
+        bool back = Input.GetKey(KeyCode.S);
+        bool forth = Input.GetKey(KeyCode.W);
+
+        if (right) gameObject.move(ext.Direction.RIGHT);
+        if (left) gameObject.move(ext.Direction.LEFT);
+        if (back) gameObject.move(ext.Direction.BACK);
+        if (forth) gameObject.move(ext.Direction.FORTH);
+    }
+}
+
+public static class ext
+{
+    public enum Direction
+    {
+        RIGHT, LEFT, UP, DOWN, BACK, FORTH
+    }
+    public static void move (this GameObject obj, Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.BACK:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y,
+                        obj.transform.position.z - walker.speed
+                    );
+                    break;
+                }
+            case Direction.FORTH:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y,
+                        obj.transform.position.z + walker.speed
+                    );
+                    break;
+                }
+            case Direction.LEFT: 
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x - walker.speed,
+                        obj.transform.position.y,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.RIGHT:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x + walker.speed,
+                        obj.transform.position.y,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.UP:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y + walker.speed,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+            case Direction.DOWN:
+                {
+                    obj.transform.position = new Vector3(
+                        obj.transform.position.x,
+                        obj.transform.position.y - walker.speed,
+                        obj.transform.position.z
+                    );
+                    break;
+                }
+        }
+    }
+}
